3c67f97f9d547fef795f2700713ca121cd7eab04
Javier Suárez <javiersuarsan@gmail.com>
2024-02-03 21:20:24 -0500

    [feature] part1.6

--- part1/unicafe/src/App.jsx
+++ part1/unicafe/src/App.jsx
@@ -6,9 +6,29 @@ const App = () => {
   const [neutral, setNeutral] = useState(0)
   const [bad, setBad] = useState(0)
 
+  const goodRating = () => {
+    const updatedGoodRating = good + 1;
+    setGood(updatedGoodRating)
+  }
+  const neutralRating = () => {
+    const updatedNeutralRating = neutral + 1;
+    setNeutral(updatedNeutralRating)
+  }
+  const badRating = () => {
+    const updatedBadRating = bad + 1;
+    setBad(updatedBadRating)
+  }
+
   return (
     <div>
-      code here
+      <h2>givefeedback</h2>
+      <button onClick={goodRating}>good</button>
+      <button onClick={neutralRating}>neutral</button>
+      <button onClick={badRating}>bad</button>
+      <h2>statistics</h2>
+      <p>good {good}</p>
+      <p>neutral {neutral}</p>
+      <p>bad {bad}</p>
     </div>
   )
 }

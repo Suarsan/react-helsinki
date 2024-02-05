49f3ef005fb00524fce8a3cdcbf8413cd13c1921
Javier Suárez <javiersuarsan@gmail.com>
2024-02-04 20:42:24 -0500

    [feature] part2.7

--- part2/phonebook/src/App.jsx
+++ part2/phonebook/src/App.jsx
@@ -1,27 +1,29 @@
 import { useState } from 'react'
 
 const App = () => {
-  const [persons, setPersons] = useState([
-    { name: 'Arto Hellas' }
-  ]) 
+  const [persons, setPersons] = useState([{ name: 'Arto Hellas' }]) 
   const [newName, setNewName] = useState('')
 
-  const handleChange = (e) => {
-    setNewName(e.target.value)
-  }
+  const handleChange = (e) => setNewName(e.target.value)
 
   const handleClick = (e) => {
     e.preventDefault()
-    const updatedpersons = [ ...persons, { name: newName } ]
-    setPersons(updatedpersons)
+    if (checkIfNameAlreadyExists(persons, newName)) {
+      alert(`${newName} is already added to phonebook`)
+    } else {
+      setPersons([ ...persons, { name: newName } ])
+    }
+    setNewName('')
   }
+
+  const checkIfNameAlreadyExists = (persons, name) =>  persons.find(p => p.name === name)
 
   return (
     <div>
       <h2>Phonebook</h2>
       <form>
         <div>
-          name: <input onChange={handleChange}/>
+          name: <input value={newName} onChange={handleChange}/>
         </div>
         <div>
           <button type="submit" onClick={handleClick}>add</button>

8f5350082cb72d44df59fe08548239ed2448bd63
Javier Suárez <javiersuarsan@gmail.com>
2024-02-06 18:38:32 -0500

    [feature] part2.17

--- part2/phonebook/src/App.jsx
+++ part2/phonebook/src/App.jsx
@@ -11,11 +11,12 @@ const App = () => {
   const [newName, setNewName] = useState('')
   const [newNumber, setNewNumber] = useState('')
   const [filterValue, setFilterValue] = useState('')
-  const [message, setMessage] = useState('')
+  const [successfulMessage, setSuccessfulMessage] = useState('')
+  const [errorMessage, setErrorMessage] = useState('')
 
-  const addPerson = (person) => { personsService.add(person).then(person => setPersons([ ...persons, person ])) }
-  const refreshPersons = () => { personsService.getAll().then(persons => setPersons(persons)) }
-  useEffect(refreshPersons, [])
+  useEffect(() => { 
+    personsService.getAll().then(persons => setPersons(persons)) 
+  }, [])
 
   const handleNameChange = (e) => setNewName(e.target.value)
   const handleNumberChange = (e) => setNewNumber(e.target.value)
@@ -31,14 +32,25 @@ const App = () => {
           .update({ ...persons.find(p => p.name === newName), number: newNumber })
           .then(person => {
             setPersons(persons.map(p => p.id === person.id ? person : p))
-            setMessage(`'${person.name}' has been updated successfully`)
-            setTimeout(() => setMessage(null), 3000)
+            setSuccessfulMessage(`'${person.name}' has been updated successfully`)
+            setTimeout(() => setSuccessfulMessage(null), 3000)
+          })
+          .catch(err => {
+            setErrorMessage(err.response.statusText)
+            setTimeout(() => setErrorMessage(null), 3000)
           })
       }
     } else {
-      addPerson({ name: newName, number: newNumber, id: `${persons.length + 1}` })
-      setMessage(`'${newName}' has been added successfully`)
-      setTimeout(() => setMessage(null), 3000)
+      personsService
+        .add({ name: newName, number: newNumber, id: `${persons.length + 1}` }).then(person => {
+          setPersons([ ...persons, person ])
+          setSuccessfulMessage(`'${newName}' has been added successfully`)
+          setTimeout(() => setSuccessfulMessage(null), 3000)
+        })
+        .catch(err => {
+          setErrorMessage(err.response.statusText)
+          setTimeout(() => setErrorMessage(null), 3000)
+        })
     }
     setNewName('')
     setNewNumber('')
@@ -46,11 +58,16 @@ const App = () => {
   const handleDelete = (person) => {
     const confirmed = confirm(`Delete '${person.name}'`)
     if (confirmed) {
-      personsService.remove(person.id).then(res => {
-        setPersons(persons.filter(p => p.id !== res.id))
-        setMessage(`'${res.name}' has been deleted successfully`)
-        setTimeout(() => setMessage(null), 3000)
-      })
+      personsService.remove(person.id)
+        .then(res => {
+          setPersons(persons.filter(p => p.id !== res.id))
+          setSuccessfulMessage(`'${res.name}' has been deleted successfully`)
+          setTimeout(() => setSuccessfulMessage(null), 3000)
+        })
+        .catch(err => {
+          setErrorMessage(err.response.statusText)
+          setTimeout(() => setErrorMessage(null), 3000)
+        })
     }
   }
 
@@ -66,7 +83,8 @@ const App = () => {
         numberValue={newNumber} 
         numberOnChange={handleNumberChange} 
         addClick={handleAdd}/>
-      <Notification message={message}/>
+      <Notification message={successfulMessage} type='success'/>
+      <Notification message={errorMessage} type='error'/>
       <h2>Numbers</h2>
       <Persons 
         persons={filteredPersons} 
--- part2/phonebook/src/Notification.jsx
+++ part2/phonebook/src/Notification.jsx
@@ -1,16 +1,17 @@
-const Notification = ({ message }) => {
+const Notification = ({ message, type }) => {
 
-    const errorClass = {
-        color: 'green',
-        backgroundColor: 'rgba(0, 255, 0, .1)',
+    const styles = {
+        color: type === 'error' ? 'red' : 'green',
+        backgroundColor: type === 'error' ? 'rgba(255, 0, 0, .1)' : 'rgba(0, 255, 0, .1)',
         borderRadius: '5px',
         margin: '5px 0',
         padding: '5px'
     };
+
     if (!message) {
         return null
     }
-    return <div style={errorClass}>{ message }</div>
+    return <div style={styles}>{ message }</div>
 
 }
 
